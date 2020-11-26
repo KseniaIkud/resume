0515a1c1542ef63cd134bb84ac8276331de78ee1
Ksenia <kudriavtseva_k@mail.ru>
2020-11-26 22:03:23 +0300

    home page adaptive added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,13 +5,10 @@ const Home = () => {
     return <div className={classes.homePage}>
         <img src={photo} alt="photo" className={classes.photo}/>
         <div className={classes.information}>
-            {/*<div className={classes.title}>Hello,*/}
-            {/*    <div className={classes.subtitle}>a bit about me:</div>*/}
-            {/*</div>*/}
-            <div className={classes.link}>
-                <div className={`${classes.resume}`}>RESUME</div>
-                <div className={`${classes.works}`}>PROJECTS</div>
-                <div className={`${classes.contact}`}>CONTACT</div>
+            <div className={classes.rounds}>
+                <div className={`${classes.link} ${classes.resume}`}>RESUME</div>
+                <div className={`${classes.link} ${classes.works}`}>PROJECTS</div>
+                <div className={`${classes.link} ${classes.contact}`}>CONTACT</div>
             </div>
             <div className={classes.welcome}>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae commodi cumque deleniti, non nostrum sed. Consectetur doloremque enim inventore ipsa! Autem corporis et, illo illum ipsam nobis temporibus voluptatibus.
