ce8786b6d5d7bc68fb3c056abfb427a5752d5f4f
Ksenia <kudriavtseva_k@mail.ru>
2020-12-29 20:18:54 +0300

    about me added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import classes from './Home.module.css'
 
 const Home = () => {
-    return <div className={classes.test}>
-        home
+    return <div className={classes.content}>
+        <div className={classes.title}>
+            About Me
+        </div>
+        <div className={classes.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vel est nihil vitae, deleniti eaque iusto sunt deserunt quo cum porro, tempore blanditiis expedita accusantium temporibus officia mollitia illo in!
+            Est eius dolores minus explicabo perferendis inventore totam itaque reprehenderit atque, voluptatibus corrupti fugiat eos doloremque tenetur quasi vitae vero numquam vel debitis ducimus exercitationem. Natus excepturi odio repudiandae voluptatum.
+        </div>
     </div>
 }
 
