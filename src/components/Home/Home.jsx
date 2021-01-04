888334cc136e18f077fab7f50cc42f5f76744379
Ksenia <kudriavtseva_k@mail.ru>
2021-01-04 16:09:33 +0300

    width size changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,8 +7,7 @@ const Home = () => {
             About Me
         </div>
         <div className={classes.description}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vel est nihil vitae, deleniti eaque iusto sunt deserunt quo cum porro, tempore blanditiis expedita accusantium temporibus officia mollitia illo in!
-            Est eius dolores minus explicabo perferendis inventore totam itaque reprehenderit atque, voluptatibus corrupti fugiat eos doloremque tenetur quasi vitae vero numquam vel debitis ducimus exercitationem. Natus excepturi odio repudiandae voluptatum.
+            
         </div>
     </div>
 }
